0d5a98a12ec886f21c893a7ad9021255bb5280b4
Turqmelon <devonthome@gmail.com>
2016-03-13 18:53:40 -0700

    Fixy fixy

--- src/main/java/com/turqmelon/MelonPerms/util/Track.java
+++ src/main/java/com/turqmelon/MelonPerms/util/Track.java
@@ -3,48 +3,7 @@ package com.turqmelon.MelonPerms.util;
 /*******************************************************************************
  * Copyright (c) 2016.  Written by Devon "Turqmelon": http://turqmelon.com
  * For more information, see LICENSE.TXT.
- * <p>
- * Written by Devon "Turqmelon" - http://turqmelon.com
- * <p>
- * This code is licensed under Creative Commons Attributation-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
- * <p>
- * You are free to:
- * SHARE - copy and redistribute the material in any medium or format
- * ADAPT - remix, transform, and build upon the material
- * <p>
- * The licensor cannot revoke these freedoms as long as you follow the license terms.
- * <p>
- * Under the following terms:
- * ATTRIBUTION - You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
- * <p>
- * NONCOMMERCIAL - You may not use the material for commercial purposes.
- * <p>
- * SHAREALIKE - If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
- * <p>
- * Full License: http://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
  ******************************************************************************/
-
-/*******************************************************************************
- * Written by Devon "Turqmelon" - http://turqmelon.com
- *
- * This code is licensed under Creative Commons Attributation-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
- *
- * You are free to:
- * SHARE - copy and redistribute the material in any medium or format
- * ADAPT - remix, transform, and build upon the material
- *
- * The licensor cannot revoke these freedoms as long as you follow the license terms.
- *
- * Under the following terms:
- * ATTRIBUTION - You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
- *
- * NONCOMMERCIAL - You may not use the material for commercial purposes.
- *
- * SHAREALIKE - If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
- *
- * Full License: http://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
- ******************************************************************************/
-
 import com.turqmelon.MelonPerms.exceptions.TrackEndReachedException;
 import com.turqmelon.MelonPerms.exceptions.TrackGroupNotDefinedException;
 import com.turqmelon.MelonPerms.exceptions.TrackNoGroupsDefinedException;
